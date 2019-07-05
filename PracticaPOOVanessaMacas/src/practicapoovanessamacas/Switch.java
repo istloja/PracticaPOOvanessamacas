0cf87b4c731d2796bc60e885dd10946c4192cc6c
ITS_LOJA <ITS_LOJA@ISTLOJA09>
2019-07-05 09:53:48 -0500

    switch condional

---
+++ PracticaPOOVanessaMacas/src/practicapoovanessamacas/Switch.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicapoovanessamacas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ITS_LOJA
+ */
+public class Switch {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+         System.out.println(" Ingrese 1 2 3 4 5");
+         int valor= entrada.nextInt();
+      
+        
+        switch (valor){
+            case 1: 
+           System.out.println("Generacion del silencio"+"1930"+"hasta"+"1948");
+               
+           }
+        }
+                
+                
+     }
+            
+
+    
+
